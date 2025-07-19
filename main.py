34381d8c1a13bf3a640a871f68e46e2022220a28
Nikososos <nickywolf@live.nl>
2025-07-19 20:37:34 +0200

    toevoegen functie om te berekenen hoeveel beweging er nodig is om genoemde calorieen te verbranden

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+if not API_KEY:
+    print("api key niet gevonden")
+
 
 # App word opgebouwd in verschillende handige functies
 
